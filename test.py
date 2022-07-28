9c4ee86b047abc200463fe58e5d250b6af8f72df
madefake <59527614+learning-source@users.noreply.github.com>
2022-07-28 20:30:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -285,7 +285,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='image.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')#需要重新下载yolov5的权重文件
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
